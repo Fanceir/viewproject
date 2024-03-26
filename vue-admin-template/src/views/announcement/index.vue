4d8bfd8fb12af28e9bb7a00420518cc2a6804ffe
fanc1 <fanxu.bot@gmail.com>
2024-03-26 08:47:22 +0800

    3files

--- vue-admin-template/src/views/announcement/index.vue
+++ vue-admin-template/src/views/announcement/index.vue
@@ -1,79 +1,174 @@
 <template>
-  <div class="app-container">
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      element-loading-text="Loading"
-      border
-      fit
-      highlight-current-row
-    >
-      <el-table-column align="center" label="ID" width="95">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+	<div class="hello">
+		<h1>公告表</h1>
+		<el-row class="table-grid-content">
+			<el-col :span="18" class="grid">
+				<el-input v-model="input" placeholder="请输入搜索内容"></el-input>
+			</el-col>
+			<el-col :span="3" class="grid" :gutter="1">
+				<el-button type="success" icon="el-icon-search">搜索</el-button>
+			</el-col>
+			<el-col :span="2" class="grid" :gutter="15">
+				<el-button type="primary" @click="addMembers()">增加</el-button>
+			</el-col>
+		</el-row>
+
+		<el-table :data="tables" :stripe="true" :border="true" width="100%">
+			<el-table-column label="日期" prop="date"></el-table-column>
+			<el-table-column label="标题" prop="title"></el-table-column>
+			<el-table-column label="公告内容" prop="announcement"></el-table-column>
+			<el-table-column label="操作">
+				<template slot-scope="scope">
+					<el-button type="primary" @click="modifyData(scope.$index, scope.row)">修改</el-button>
+					<el-button type="danger" @click="deleteData(scope.$index,tableData)">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+		<el-dialog :visible.sync="centerDialogVisible">
+			<el-form :model="editForm">
+				<el-form-item label="日期" :picker-options="pickerOptions">
+					<el-date-picker v-model="editForm.date" type="date" placeholder="选择日期" value-format="yyyy-MM-dd"></el-date-picker>
+				</el-form-item>
+				<el-form-item label="标题">
+					<el-input v-model="editForm.title"></el-input>
+				</el-form-item>
+				<el-form-item label="公告内容">
+					<el-input v-model="editForm.announcement"></el-input>
+				</el-form-item>
+			</el-form>
+			<div>
+				<el-button @click="closeDialog()">取消</el-button>
+				<el-button type="primary" @click="sumbitEditRow()">确定</el-button>
+			</div>
+		</el-dialog>
+		<el-dialog :visible.sync="isAddMembers">
+			<el-form :model="addForm">
+				<el-form-item label="日期" :picker-options="pickerOptions">
+					<el-date-picker v-model="addForm.date" type="date" placeholder="选择日期" value-format="yyyy-MM-dd"></el-date-picker>
+				</el-form-item>
+				<el-form-item label="标题">
+					<el-input v-model="addForm.title"></el-input>
+				</el-form-item>
+				<el-form-item label="公告内容">
+					<el-input v-model="addForm.announcement"></el-input>
+				</el-form-item>
+			</el-form>
+			<div>
+				<el-button @click="closeDialog()">取消</el-button>
+				<el-button type="primary" @click="sumbitAddRow()">确定</el-button>
+			</div>
+		</el-dialog>
+	</div>
 </template>
 
 <script>
-import { getList } from '@/api/table'
+	// import { musicBroadcastingDetails } from '@/api/index.js'
+	import axios from 'axios'
+	var _index;
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App',
+				centerDialogVisible: false,
+				isAddMembers: false,
+				editForm: [],
+				addForm: [],
+				searchData: '',
+				input: '',
+				id:'',
+				pickerOptions: {
+					disabledDate(time) {
+						return time.getTime() > Date.now();
+					}
+				},
+				tableData: [{
+						date: '2024-03-02',
+						title: '图书馆开放时间',
+						announcement: '图书馆开放时间为8:00-20:00'
+					}, {
+						date: '2024-03-04',
+						title: '教学楼开放时间',
+						announcement: '教学楼开放时间为6:00-22:00'
+					}
+				]
+			}
+		},
+		methods: {
+			deleteData(index, row) {
+				this.tableData.splice(index, 1)
+				console.log("进行了删除操作")
+				console.log("index的值是：" + index)
+				console.log("row的值是：", row)
+			},
+			modifyData(index, row) {
+				this.centerDialogVisible = true
+				this.editForm = Object.assign({}, row); //重置对象
+				_index = index;
+				console.log("index的值：" + index)
+				console.log("_index的值：" + _index)
+				console.log("row的值是：", this.editForm) //代表选择的这一行的数据
+			},
+			sumbitEditRow() {
+				var editData = _index;
+				console.log("editData的值" + this.editForm)
+				this.tableData[editData].title = this.editForm.title;
+				this.tableData[editData].date = this.editForm.date;
+				this.tableData[editData].announcement = this.editForm.announcement;
+				this.centerDialogVisible = false;
+				console.log("数据：" + this.editForm.date)
+				console.log("对象数组", this.tableData)
+			},
+			closeDialog() {
+				this.centerDialogVisible = false
+				this.isAddMembers = false
+				console.log("editfrom", this.editForm)
+				console.log("addfrom", this.addForm)
 
-export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
-  data() {
-    return {
-      list: null,
-      listLoading: true
-    }
-  },
-  created() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData() {
-      this.listLoading = true
-      getList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
-    }
-  }
-}
+			},
+			addMembers() {
+				this.isAddMembers = true
+				this.addForm = {
+					data: '',
+					title: '',
+					announcement: ''
+				}
+			},
+			sumbitAddRow() {
+				this.tableData = this.tableData || []
+				console.log("表格是:" + this.tableData)
+				this.tableData.push({
+					title: this.addForm.title,
+					date: this.addForm.date,
+					announcement: this.addForm.announcement
+				})
+				this.isAddMembers = false;
+				console.log("新增的日期：" + this.addForm.date)
+			}
+		},
+		computed: {
+			tables() {
+				const input = this.input
+				if (input) {
+					console.log("input输入的搜索内容：" + this.input)
+					return this.tableData.filter(data => {
+						console.log("object:" + Object.keys(data))
+						return Object.keys(data).some(key => {
+							return String(data[key]).toLowerCase().indexOf(input) > -1
+						})
+					})
+				}
+				return this.tableData
+			}
+		}
+	}
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.table-grid-content {
+		border-radius: 4px;
+		height: 50 px;
+		background: #ebeef5;
+		padding: 10px;
+	}
+</style>
--- vue-admin-template/src/views/dashboard/index.vue
+++ vue-admin-template/src/views/dashboard/index.vue
@@ -1,97 +1,310 @@
 <template>
-  <div class="dashboard-container">
-    <h1>Dashboard</h1>
-    <div class="chart" ref="chart1"></div>
-    <div class="chart" ref="chart2"></div>
-  </div>
-
+	<div class="dashboard-container">
+		<h1>Dashboard</h1>
+		<div ref="chart1" class="chart" />
+		<div ref="chart2" class="chart" />
+		<div ref="chart3" class="chart" />
+		<div ref="chart4" class="chart" />
+	</div>
 </template>
 
 <script>
-// 引入 Echarts 库
-import * as echarts from "echarts";
-
+import * as echarts from 'echarts'
 export default {
-  name: 'Dashboard',
-  data() {
-    return {
-      // 存储图表实例
-      charts: [],
-      // 自定义数据
-      customData: {
-        chart1: [30, 40, 50, 60, 70],
-        chart2: [70, 60, 90, 40, 45],
-        // 其他图表数据...
-      }
-    };
-  },
-  mounted() {
-    // 在组件挂载完成后渲染图表
-    this.renderCharts();
-  },
-  methods: {
-    renderCharts() {
-      // 获取图表容器元素
-      const chart1 = this.$refs.chart1;
-      const chart2 = this.$refs.chart2;
-      // 其他图表容器...
+	/*eslint-disable*/
+	// eslint-disable-next-line
+	name: "Dashboard",
+	data() {
+		return {
+			// 存储图表实例
+			charts: [],
+			// 自定义数据
+			customData: {
+				chart1: [30, 40, 50, 60, 70],
+				chart2: [70, 60, 90, 40, 45],
+				chart3: [
+					{ value: 23, name: "大一" },
+					{ value: 24, name: "大二" },
+					{ value: 18, name: "大三" },
+					{ value: 25, name: "大四" }
+				],
+				chart4: [23, 24, 18, 25, 27, 28, 25]
+				// 其他图表数据...
+			}
+		};
+	},
+	mounted() {
+		//颜色选项
+		let colors = ['#5470c6', '#91cc75', '#fac858', '#ee6666', '#73c0de', '#3ba272']
+		// 在组件挂载完成后渲染图表
+		this.renderCharts();
+		// 监听窗口大小变化，重新渲染图表
+		window.addEventListener("resize", this.handleResize);
+		// 设置定时器，每隔两秒更新数据
+		setInterval(() => {
+			this.updateChartData();
+		}, 2000);
+	},
+	beforeDestroy() {
+		// 在组件销毁前移除窗口大小变化监听器
+		window.removeEventListener("resize", this.handleResize);
+	},
+	methods: {
+		renderCharts() {
+			// 获取图表容器元素
+			const chart1 = this.$refs.chart1;
+			const chart2 = this.$refs.chart2;
+			const chart3 = this.$refs.chart3;
+			const chart4 = this.$refs.chart4;
+			// 使用 Echarts 初始化图表实例
+			const myChart1 = echarts.init(chart1);
+			const myChart2 = echarts.init(chart2);
+			const myChart3 = echarts.init(chart3);
+			const myChart4 = echarts.init(chart4);
+			// 配置图表数据...
+			myChart1.setOption({
+				// 表名
+				title: { text: "注册人数" },
+				// 横轴
+				xAxis: {
+					type: "category",
+					data: ["大一", "大二", "大三", "大四", "教师"],
+				},
+				// 纵轴
+				yAxis: {
+					type: "value",
+				},
+				// 数据来源、图表类型
+				series: [
+					{
+						data: this.customData.chart1,
+						type: "bar",
+					},]
+			});
+			// 其他图表配置
+			//折线图
+			myChart2.setOption({
+				title: {
+					text: '任务完成数量',
+				},
+				xAxis: {
+					type: 'category',
+					data: ['任务1', '任务2', '任务3', '任务4', '任务5'],
+				},
+				yAxis: {
+					type: 'value',
+				},
+				series: [{
+					data: this.customData.chart2,
+					type: 'line',
+					//颜色设置
+					color: ['#00aa00', '#F44042']
+					//itemStyle : {normal : {color:'#409EFF'}, //改变折线点的颜色
+					//lineStyle:{color:'#00aa00'}}//改变折线颜色
+				}],
+			});
+			//饼图
+			myChart3.setOption({
+				//图例位置属性设置
+				legend: {
+					right: "10%",
+					top: "30%",
+					orient: "vertical"
+				},
+				title: {
+					text: '各年级占比',
+					top: "0%",
+					//设置标题居中
+					left: "center"
+				},
+				series: [{
+					//自己设定的数据
+					data: this.customData.chart3,
+					//随机生成数据
+					//data:makeRandomData(),
+					type: 'pie',
+					//图例
+					label: {
+						show: true,
+						//设置图例的表示形式
+						formatter: "{b}:{c}({d}%)"
+					},
+					//饼图的大小
+					radius: "80%",
+				}]
+			});
+			//散点图
+			myChart4.setOption({
+				title: {
+					text: '各天在线人数(周次)'
+				},
+				xAxis: {
+					type: 'category',
+					data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+				},
+				yAxis: {
+					type: 'value'
+				},
+				series: [{
+					data: this.customData.chart4,
+					type: 'scatter',
+					color: '#409EFF'
+				}]
+			});
+			//添加数据缩放和工具栏功能
+			myChart1.setOption({
+				dataZoom: [
+					{
+						type: 'slider', // 缩放类型为滑动条
+						start: 0, // 起始位置百分比
+						end: 100 // 结束位置百分比
+					},
+					{
+						type: 'inside' // 内部缩放
+					}
+				],
+				toolbox: {
+					feature: {
+						dataView: { readOnly: false }, // 数据视图，用户可以查看数据
+						saveAsImage: {} // 导出图片
+					}
+				},
+				tooltip: {
+					trigger: 'axis' // 鼠标悬停提示
+				}
+			});
 
-      // 使用 Echarts 初始化图表实例
-      const myChart1 = echarts.init(chart1);
-      const myChart2 = echarts.init(chart2);
-      // 其他图表实例...
+			myChart2.setOption({
+				dataZoom: [
+					{
+						type: 'slider', // 缩放类型为滑动条
+						start: 0, // 起始位置百分比
+						end: 100 // 结束位置百分比
+					},
+					{
+						type: 'inside' // 内部缩放
+					}
+				],
+				toolbox: {
+					feature: {
+						dataView: { readOnly: false }, // 数据视图，用户可以查看数据
+						saveAsImage: {} // 导出图片
+					}
+				},
+				tooltip: {
+					trigger: 'axis' // 鼠标悬停提示
+				}
+			});
 
-      // 配置图表数据
-      myChart1.setOption({
-        title: {
-          text: '注册人数'
-        },
-        xAxis: {
-          type: 'category',
-          data: ['大一', '大二', '大三', '大四', '教师']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: this.customData.chart1,
-          type: 'bar'
-        }]
-      });
-
-      myChart2.setOption({
-        title: {
-          text: '任务完成数量'
-        },
-        xAxis: {
-          type: 'category',
-          data: ['任务1', '任务2', '任务3', '任务4', '任务5']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: this.customData.chart2,
-          type: 'line'
-        }]
-      });
-
-      // 存储图表实例
-      this.charts.push(myChart1, myChart2);
-      // 其他图表实例...
-    }
-  }
+			myChart4.setOption({
+				dataZoom: [
+					{
+						type: 'slider', // 缩放类型为滑动条
+						start: 0, // 起始位置百分比
+						end: 100 // 结束位置百分比
+					},
+					{
+						type: 'inside' // 内部缩放
+					}
+				],
+				toolbox: {
+					feature: {
+						dataView: { readOnly: false }, // 数据视图，用户可以查看数据
+						saveAsImage: {} // 导出图片
+					}
+				},
+				tooltip: {
+					trigger: 'axis' // 鼠标悬停提示
+				}
+			});
+			// 存储图表实例
+			this.charts.push(myChart1, myChart2);
+			this.charts.push(myChart3, myChart4);
+			// 其他图表实例...
+		},
+		updateChartData() {
+			// 随机生成不同类型的新数据并更新图表
+			this.charts.forEach((chart, index) => {
+				let newData;
+				switch (index) {
+					case 0:
+						newData = this.makeRandomData('myChart1');
+						break;
+					case 1:
+						newData = this.makeRandomData('myChart2');
+						break;
+					case 2:
+						newData = this.makeRandomData('myChart3');
+						break;
+					case 3:
+						newData = this.makeRandomData('myChart4');
+						break;
+					default:
+						newData = this.makeRandomData('default');
+				}
+				chart.setOption({
+					series: [{ data: newData }]
+				});
+			});
+		},
+		makeRandomData(chartType) {
+			switch (chartType) {
+				case 'myChart1':
+					return [
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) }
+					];
+				case 'myChart2':
+					return [
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) }
+					];
+				case 'myChart3':
+					return [
+						{ value: Math.floor(Math.random() * 10000), name: "大一" },
+						{ value: Math.floor(Math.random() * 10000), name: "大二" },
+						{ value: Math.floor(Math.random() * 10000), name: "大三" },
+						{ value: Math.floor(Math.random() * 10000), name: "大四" }
+					];
+				case 'myChart4':
+					return [
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) },
+						{ value: Math.floor(Math.random() * 10000) }
+					];
+				default:
+					return [];
+			}
+		},
+		handleResize() {
+			// 窗口大小变化时重新渲染图表
+			this.charts.forEach(chart => {
+				chart.resize();
+			});
+		}
+	}
 };
 </script>
 
 <style scoped>
 .dashboard {
-  display: flex;
-  flex-wrap: wrap;
+	display: flex;
+	flex-wrap: wrap;
 }
 
 .chart {
-  width: 50%;
-  height: 400px;
+	float: left;
+	width: 100%;
+	height: 400px;
 }
 </style>
